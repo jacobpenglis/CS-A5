afc0dee7795b60772667f7108fa74227f8fc0db7
Jacob Penglis <jacobpenglis@jacobs-air.home>
2024-09-09 15:19:22 +0930

    Struggling to implement mult

--- prac5/abs/Abs.asm
+++ prac5/abs/Abs.asm
@@ -2,3 +2,23 @@
 // (R0, R1 refer to RAM[0], and RAM[1], respectively.)
 
 // Put your code here.
+@R1
+D=M
+@POS        // (A=[POS]) Conditional IF (Memory of R1 > 0), no need to change
+D;JGE       // >= 0
+@R1         // Otherwise load R1 into A reg again
+D=-M
+@R0
+M=D
+@END
+0;JMP
+
+(POS)       // If positive, just store
+@R0
+M=D
+@END
+0;JMP
+
+(END)       // loop until fin
+@END
+0;JMP
--- prac5/mult/Mult.asm
+++ prac5/mult/Mult.asm
@@ -7,3 +7,57 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@R0         // Initialise R0
+M=0
+
+@R1         // Check if r1 val is 0
+D=M
+@END
+D;JEQ
+
+@R2         // Check if r2 val is 0
+D=M
+@END
+D;JEQ
+
+// if either val is 0, don't change output R0
+
+@R2          // Check if R2 val if negative
+D=M
+@POS
+D;JGT
+
+D=-D        // Negate negative val
+D=D+1
+
+(POS)       // Regardless/Otherwise, initialise, INDEX, or, how many times multiplication needs to occur
+@INDEX
+M=D
+
+(LOOP)      // Main multiplication loop through addition, decrement index in each loop
+@R1
+D=M
+@R0
+M=M+D
+@INDEX
+M=M-1
+D=M
+@LOOP
+D;JGT
+
+@R1         // Check if R1 is negative (<0)
+D=M
+@NEGATE
+D;JLT
+
+(END)
+@END
+0;JMP
+
+(NEGATE)    // Negate final result
+@R0
+M=-M
+M=M+1
+@END
+0;JMP
